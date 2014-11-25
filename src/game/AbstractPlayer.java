dc6a91112618a11279f1c6cc6444fe6623d338eb
Michael Patashnik <Mgpshnik@gmail.com>
2014-11-24 22:23:04 -0500

    Added abstractPlayer stub for use in unit classes

---
+++ src/game/AbstractPlayer.java
@@ -0,0 +1,10 @@
+package game;
+
+/** An instance is a player (not the commander piece).
+ * Extended to be either human controlled or AI.
+ * @author MPatashnik
+ *
+ */
+public abstract class AbstractPlayer {
+
+}
---
+++ src/game/package-info.java
@@ -0,0 +1,8 @@
+/** Holder for game controlling classes. Most are not visually drawn in any way.
+ * 
+ */
+/**
+ * @author MPatashnik
+ *
+ */
+package game;
