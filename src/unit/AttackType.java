9d2a5997ce38651f9ebe4b3287672b3d122fea34
Michael Patashnik <Mgpshnik@gmail.com>
2014-12-06 22:55:11 -0500

    Added attack type abbreviation converters

--- src/unit/AttackType.java
+++ src/unit/AttackType.java
@@ -5,5 +5,27 @@ public enum AttackType {
 	NO_ATTACK,
 	PHYSICAL,
 	MAGIC,
-	TRUE
+	TRUE;
+	
+	/** Returns the attack type from the given abbreviated (one char) string.
+	 * @param s - either "p" - Physical, "m" - Magic, "t" - true, or anything else - NO_ATTACK
+	 * @return
+	 */
+	public AttackType fromAbbrevString(String s){
+		switch(s.toLowerCase()){
+		case "p": return PHYSICAL;
+		case "m": return MAGIC;
+		case "t": return TRUE;
+		default: return NO_ATTACK;
+		}
+	}
+	
+	public String getAbbrevString(AttackType t){
+		switch(t){
+		case MAGIC:		return "m";
+		case PHYSICAL:	return "p";
+		case TRUE:		return "t";
+		default:		return "n";
+		}
+	}
 }
