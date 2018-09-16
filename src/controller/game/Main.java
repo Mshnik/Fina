d37c16ed9bf5af2a940617cae9a58ac5b40bb6f0
Michael Patashnik <mgpshnik@gmail.com>
2018-09-16 12:58:59 -0700

    Added truly massive board for stress testing / better results on profiling

--- src/controller/game/Main.java
+++ src/controller/game/Main.java
@@ -13,18 +13,18 @@ import model.unit.combatant.Combatants;
 public final class Main {
   /** Simple main method to test out Frame features */
   public static void main(String[] args) throws InterruptedException {
-    Thread.sleep(0);
+    Thread.sleep(10000);
     // Force unit, building, spell, audio loading.
     Combatants.getCombatantsForAge(1);
     Buildings.getBuildingsForLevel(1);
     Abilities.getAbilitiesForAge(1);
 
     // Select initial board file and make start game.
-    String boardFilename = args.length > 0 ? args[0] : "Crater.csv";
+    String boardFilename = args.length > 0 ? args[0] : "Expanse.csv";
     List<String> defaultPlayerTypes = new ArrayList<>();
-    defaultPlayerTypes.add(HUMAN_PLAYER_TYPE);
-    defaultPlayerTypes.add(HUMAN_PLAYER_TYPE);
+    defaultPlayerTypes.add(FULL_RANDOM_AI_TYPE);
+    defaultPlayerTypes.add(FULL_RANDOM_AI_TYPE);
     GameController.loadAndStart(
-        "game/boards/" + boardFilename, defaultPlayerTypes, FogOfWar.REGULAR, 1, 10, 10, 3);
+        "game/boards/" + boardFilename, defaultPlayerTypes, FogOfWar.REGULAR, 1, 9, 18, 3);
   }
 }
