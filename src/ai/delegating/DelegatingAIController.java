d4090acd06c0a59746c42fcd5358406625e6a688
Michael Patashnik <mgpshnik@gmail.com>
2018-09-18 19:02:19 -0700

    Add Delegating controller

---
+++ src/ai/delegating/DelegatingAIController.java
@@ -0,0 +1,14 @@
+package ai.delegating;
+
+import ai.AIAction;
+import ai.AIController;
+import model.game.Player;
+
+/** An AI controller that maintains a set of delegates to determine its behavior. */
+public final class DelegatingAIController implements AIController {
+
+  @Override
+  public AIAction getNextAction(Player player) {
+    return null;
+  }
+}
