23ff3f6386fcf013ee7344a842c093d352d47fc3
Michael Patashnik <mgpshnik@gmail.com>
2018-10-24 13:44:40 -0700

    Add test dummies

---
+++ tests/model/board/TerrainTest.java
@@ -0,0 +1,12 @@
+package model.board;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public final class TerrainTest {
+
+  @Test
+  public void test() {}
+}
---
+++ tests/model/util/ExpandableCloudTest.java
@@ -0,0 +1,12 @@
+package model.util;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public final class ExpandableCloudTest {
+
+  @Test
+  public void test() {}
+}
